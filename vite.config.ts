5d8b34e6a8323aca0f7efc3f49aee6f9a0655978
tgt <tgt@incognitotgt.me>
2025-02-04 22:45:46 -0500

    fix: try to fix pnpm ver

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()],
-  server: {
-    allowedHosts: ['.ts.net']
-  },
-  ssr: {
-    noExternal: ["@icons-pack/svelte-simple-icons"]
-  }
+	plugins: [sveltekit()],
+	server: {
+		allowedHosts: ['.ts.net']
+	},
+	ssr: {
+		noExternal: ['@icons-pack/svelte-simple-icons']
+	}
 });
